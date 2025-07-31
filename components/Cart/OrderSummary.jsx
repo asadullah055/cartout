edc24932e031ffc5f836f04e42b2ff57bcbf8e9d
asadullah055 <asadullahahmed01@gmail.com>
2025-07-31 17:14:57 +0600

    add order summary

--- components/Cart/OrderSummary.jsx
+++ components/Cart/OrderSummary.jsx
@@ -1,38 +1,41 @@
 // OrderSummary.jsx
 
+import { IoLocationOutline } from "react-icons/io5";
+
 const OrderSummary = () => (
   <div className="bg-white rounded p-2">
     <div className="mb-4">
-      <div className="text-sm text-gray-500">Location</div>
-      <div className="font-semibold">
-        Gomastapur, Chapai Nawabganj, Rajshahi
+      <div className="text-sm text-gray-500 font-medium">Location</div>
+      <div className="font-semibold flex gap-x-3 mt-2 text-[12px] text-gray-600">
+        <IoLocationOutline size={22} className="text-gray-700" /> Gomastapur,
+        Chapai Nawabganj, Rajshahi
       </div>
     </div>
     <div className="mb-4">
-      <div className="font-semibold mb-2">Order Summary</div>
-      <div className="flex justify-between text-sm mb-1">
-        <span>Subtotal (0 items)</span>
+      <div className="font-medium text-lg mb-2">Order Summary</div>
+      <div className="flex justify-between text-sm mb-2">
+        <span className="text-gray-600">Subtotal (0 items)</span>
         <span>৳ 0</span>
       </div>
-      <div className="flex justify-between text-sm mb-1">
-        <span>Shipping Fee</span>
+      <div className="flex justify-between text-sm mb-2">
+        <span className="text-gray-600">Shipping Fee</span>
         <span>৳ 0</span>
       </div>
-      <div className="flex gap-2 mt-2">
+      <div className="flex justify-between gap-2 mt-2">
         <input
           type="text"
           placeholder="Enter Voucher Code"
-          className="border rounded px-2 py-1 flex-1"
+          className="border rounded px-2 py-1 w-full "
         />
-        <button className="bg-blue-500 text-white px-4 rounded">APPLY</button>
+        <button className="bg-orange-500 text-white px-4 rounded">APPLY</button>
       </div>
     </div>
-    <div className="flex justify-between font-bold text-lg mb-4">
+    <div className="flex justify-between text-md mb-4">
       <span>Total</span>
-      <span>৳ 0</span>
+      <span className="text-orange-500">৳ 1237 </span>
     </div>
-    <button className="w-full bg-orange-500 text-white py-2 rounded font-semibold">
-      PROCEED TO CHECKOUT (0)
+    <button className="w-full bg-orange-500 text-white py-2 rounded">
+      Process Checkout (2)
     </button>
   </div>
 );
